nxp-qoriq-yocto-sdk/pkc-firmware

commit baa8a25d9693c0107b90b4bb0389733cb7450faa

remove unnecessary type definition "sec_jr_regs_t"
Change-Id: Ib59a357d6d3c69fa2d6135037ca096e3bb9380a4
Signed-off-by: Radu Alexe <raduandrei.alexe@freescale.com>

c2x0_boot_loader/fsl_c2x0_fw.c
@@ -613,7 +613,7 @@ static void sec_eng_hw_init(struct sec_engine *sec)
 #define JRREGS_OFFSET   0x1000
 #define C2X0_CONFIG_SYS_FSL_JR0_ADDR \
 	(CCSR_VIRT_ADDR + SEC0_OFFSET + JRREGS_OFFSET)
-	sec->jr.regs = (sec_jr_regs_t *) C2X0_CONFIG_SYS_FSL_JR0_ADDR;
+	sec->jr.regs = (struct sec_jr_regs *) C2X0_CONFIG_SYS_FSL_JR0_ADDR;
 
 #define C2X0_SECINFO_ADDR       (CCSR_VIRT_ADDR + SEC0_OFFSET)
 	sec->info = (ccsr_sec_t *) C2X0_SECINFO_ADDR;
@@ -756,7 +756,7 @@ static void init_sec_regs_offset(struct sec_engine *sec)
 #define SEC0_OFFSET				0x80000
 #define C2X0_SEC0_JR0_ADDR	(CCSR_VIRT_ADDR + SEC0_OFFSET + JRREGS_OFFSET)
 #define C2X0_SEC0_INFO_ADDR	(CCSR_VIRT_ADDR + SEC0_OFFSET)
-		sec->jr.regs = (sec_jr_regs_t *) C2X0_SEC0_JR0_ADDR;
+		sec->jr.regs = (struct sec_jr_reg *) C2X0_SEC0_JR0_ADDR;
 		sec->info = (ccsr_sec_t *) C2X0_SEC0_INFO_ADDR;
 		sec->rng =
 		    (rng_regs_t *) (CCSR_VIRT_ADDR + SEC0_OFFSET + 0x00600);
@@ -769,7 +769,7 @@ static void init_sec_regs_offset(struct sec_engine *sec)
 #define SEC1_OFFSET				0xa0000
 #define C2X0_SEC1_JR0_ADDR      (CCSR_VIRT_ADDR + SEC1_OFFSET + JRREGS_OFFSET)
 #define C2X0_SEC1_INFO_ADDR     (CCSR_VIRT_ADDR + SEC1_OFFSET)
-		sec->jr.regs = (sec_jr_regs_t *) C2X0_SEC1_JR0_ADDR;
+		sec->jr.regs = (struct sec_jr_regs *) C2X0_SEC1_JR0_ADDR;
 		sec->info = (ccsr_sec_t *) C2X0_SEC1_INFO_ADDR;
 		sec->rng =
 		    (rng_regs_t *) (CCSR_VIRT_ADDR + SEC1_OFFSET + 0x00600);
@@ -782,7 +782,7 @@ static void init_sec_regs_offset(struct sec_engine *sec)
 #define SEC2_OFFSET             0xc0000
 #define C2X0_SEC2_JR0_ADDR      (CCSR_VIRT_ADDR + SEC2_OFFSET + JRREGS_OFFSET)
 #define C2X0_SEC2_INFO_ADDR     (CCSR_VIRT_ADDR + SEC2_OFFSET)
-		sec->jr.regs = (sec_jr_regs_t *) C2X0_SEC2_JR0_ADDR;
+		sec->jr.regs = (struct sec_jr_regs *) C2X0_SEC2_JR0_ADDR;
 		sec->info = (ccsr_sec_t *) C2X0_SEC2_INFO_ADDR;
 		sec->rng =
 		    (rng_regs_t *) (CCSR_VIRT_ADDR + SEC2_OFFSET + 0x00600);

c2x0_boot_loader/fsl_c2x0_fw.h
@@ -136,7 +136,7 @@ typedef unsigned long i64;
 /* 32 bit virtual address */
 typedef u32 va_addr_t;
 
-typedef struct sec_jr_regs {
+struct sec_jr_regs {
 	u32 irba_h;
 	u32 irba_l;
 	u32 rsvd1;
@@ -165,7 +165,7 @@ typedef struct sec_jr_regs {
 	u32 orwi;
 	u32 rsvd11;
 	u32 jrcr;
-} sec_jr_regs_t;
+};
 
 struct sec_ip_ring {
 	dma_addr_t desc;
@@ -177,7 +177,7 @@ struct sec_op_ring {
 } __packed;
 
 struct sec_jr {
-	sec_jr_regs_t *regs;
+	struct sec_jr_regs *regs;
 	u32 size;
 	u32 head;
 	u32 tail;

commit fa58ed6ed5e5811ec192816942e0feaafc0abe0a

fix typo regarding the use of "struct sec_jr_regs"
Typos introduced in commit baa8a25d9693c0107b90b4bb0389733cb7450faa

Signed-off-by: Radu Alexe <raduandrei.alexe@freescale.com>

c2x0_boot_loader/fsl_c2x0_fw.c
@@ -777,7 +777,7 @@ static void init_sec_regs_offset(struct sec_engine *sec)
 
 	switch (id) {
 	case SEC_ENG_1:
-		sec->jr.regs = (struct sec_jr_reg *) SEC0_JR0_ADDR;
+		sec->jr.regs = (struct sec_jr_regs *) SEC0_JR0_ADDR;
 		sec->info = (ccsr_sec_t *) SEC0_BASE_ADDR;
 		sec->rng = (struct rng_regs *) SEC0_RNG_ADDR;
 		sec->kek = (struct kek_regs *) SEC0_KEK_ADDR;
@@ -786,7 +786,7 @@ static void init_sec_regs_offset(struct sec_engine *sec)
 		break;
 
 	case SEC_ENG_2:
-		sec->jr.regs = (struct sec_jr_reg *) SEC1_JR0_ADDR;
+		sec->jr.regs = (struct sec_jr_regs *) SEC1_JR0_ADDR;
 		sec->info = (ccsr_sec_t *) SEC1_BASE_ADDR;
 		sec->rng = (struct rng_regs *) SEC1_RNG_ADDR;
 		sec->kek = (struct kek_regs *) SEC1_KEK_ADDR;
@@ -795,7 +795,7 @@ static void init_sec_regs_offset(struct sec_engine *sec)
 		break;
 
 	case SEC_ENG_3:
-		sec->jr.regs = (struct sec_jr_reg *) SEC2_JR0_ADDR;
+		sec->jr.regs = (struct sec_jr_regs *) SEC2_JR0_ADDR;
 		sec->info = (ccsr_sec_t *) SEC2_BASE_ADDR;
 		sec->rng = (struct rng_regs *) SEC2_RNG_ADDR;
 		sec->kek = (struct kek_regs *) SEC2_KEK_ADDR;