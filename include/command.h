090755ea86e6cd044b39e8abfddc5ccf9e5e264e
Cristian Stoica <cristian.stoica@nxp.com>
2016-09-21 16:02:50 +0300

    remove not used structure cmd_tbl_s
    Signed-off-by: Cristian Stoica <cristian.stoica@nxp.com>

--- include/command.h
+++ include/command.h
@@ -39,25 +39,6 @@
 #endif
 
 #ifndef	__ASSEMBLY__
-/*
- * Monitor Command Table
- */
-
-struct cmd_tbl_s {
-	char		*name;		/* Command Name			*/
-	int		maxargs;	/* maximum number of arguments	*/
-	int		repeatable;	/* autorepeat allowed?		*/
-					/* Implementation function	*/
-	int		(*cmd)(struct cmd_tbl_s *, int, int, char * const []);
-	char		*usage;		/* Usage message	(short)	*/
-#ifdef	CONFIG_SYS_LONGHELP
-	char		*help;		/* Help  message	(long)	*/
-#endif
-#ifdef CONFIG_AUTO_COMPLETE
-	/* do auto completion on the arguments */
-	int		(*complete)(int argc, char * const argv[], char last_char, int maxv, char *cmdv[]);
-#endif
-};
 
 typedef struct cmd_tbl_s	cmd_tbl_t;
 
